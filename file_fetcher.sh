33615fc88c1af4297dacda9f2dbd198e5387d396
Gabriel Perdue <perdue@fnal.gov>
2014-07-24 16:55:56 -0500

    Move the file fetcher.

---
+++ file_fetcher.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+filelist='coh_carbon_splines_RS280.xml coh_carbon_splines_RSDEV4141.xml coh_carbon_splines_BSDEV4141.xml'
+
+for filenam in $filelist
+do
+  scp perdue@minervagpvm02.fnal.gov:/minerva/app/users/perdue/GENIE/runs/${filenam} ./splines
+done
+
+scp perdue@minervagpvm02.fnal.gov:/minerva/app/users/perdue/GENIE/runs/gntp.berger_sehgal.gst.root ./data
